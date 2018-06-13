9b50db0b871b092e184dc0767018bfd17932768b
Cam Davies <pawn@live.com>
2018-06-13 11:23:09 -0600

    UPDATE

--- app/src/main/java/com/seng/taskify/MainActivity.java
+++ app/src/main/java/com/seng/taskify/MainActivity.java
@@ -1,7 +1,10 @@
 package com.seng.taskify;
 
+
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // when hit add task
+    public void onClickAddTask(View view){
+        ConstraintLayout cl = findViewById(R.id.clAddTasks);
+        hideLayouts();
+        cl.setVisibility(View.VISIBLE);
+    }
+
+    // when press home button, return button, or finished button
+    public void onClickHome(View view){
+        ConstraintLayout cl = findViewById(R.id.clListTasks);
+        hideLayouts();
+        cl.setVisibility(View.VISIBLE);
+    }
+
+    // When click categories
+    public void onClickCategories(View view){
+        ConstraintLayout cl = findViewById(R.id.clCategories);
+        hideLayouts();
+        cl.setVisibility(View.VISIBLE);
+    }
+
+    // When click completed
+    public void onClickCompleted(View view){
+        ConstraintLayout cl = findViewById(R.id.clCompletedTasks);
+        hideLayouts();
+        cl.setVisibility(View.VISIBLE);
+    }
+
+    private void hideLayouts(){
+        ConstraintLayout cl;
+        cl = findViewById(R.id.clAddTasks);
+        cl.setVisibility(View.INVISIBLE);
+        cl = findViewById(R.id.clListTasks);
+        cl.setVisibility(View.INVISIBLE);
+        cl = findViewById(R.id.clCategories);
+        cl.setVisibility(View.INVISIBLE);
+        cl = findViewById(R.id.clCompletedTasks);
+        cl.setVisibility(View.INVISIBLE);
+    }
+
+
 }
